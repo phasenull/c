0d0bb1e6eb497d0983172381cc2961effa289ac9
phasenull <55253035+phasenull@users.noreply.github.com>
2026-06-05 00:02:34 +0300

    csv loading and parsing

--- 3-file-exp/main.c
+++ 3-file-exp/main.c
@@ -3,7 +3,7 @@
 #include "stdlib.h"
 #include "stdio.h"
 #define FILE_PATH "./transactions.csv"
-#define BYTES_PER_COLUMN 256
+#define BYTES_PER_COLUMN 32
 #define MAX_COLUMNS 10
 struct DataRow
 {
@@ -11,23 +11,78 @@ struct DataRow
 };
 struct ApplicationMemory
 {
-	int *row_data;
-	int *column_names;
+	char *row_data;
+	char *column_names;
+	int column_count;
+	int row_count;
 };
+void printStringArray(char *pointer)
+{
+	int i = 0;
 
+	while (1)
+	{
+		if (pointer[0] == '\0')
+		{
+			break;
+		}
+
+		printf("%d: %s\n", i, pointer);
+		pointer = pointer + BYTES_PER_COLUMN;
+		i++;
+	}
+}
 int loadFile(FILE *stream, struct ApplicationMemory *app)
 {
 	fseek(stream, 0, SEEK_SET);
-	char *read = malloc(BYTES_PER_COLUMN * MAX_COLUMNS);
-	if (!read)
+	app->column_names = malloc(BYTES_PER_COLUMN * MAX_COLUMNS);
+	char *buffer = malloc(BYTES_PER_COLUMN * MAX_COLUMNS);
+	if (!buffer)
 	{
 		exit(1);
 	}
-	fgets(read, BYTES_PER_COLUMN * MAX_COLUMNS, stream);
-	printf("Column row: %s", read);
+	if (!app->column_names)
+	{
+		exit(1);
+	}
+	fgets(buffer, BYTES_PER_COLUMN * MAX_COLUMNS, stream);
+	char *reader = buffer;
+	for (int i = 0; i < MAX_COLUMNS; i++)
+	{
+		reader = strtok(i == 0 ? reader : NULL, "|");
+		if (!reader)
+		{
+			app->column_count = i;
+			break;
+		}
+		strcpy(app->column_names + BYTES_PER_COLUMN * i, reader);
+	}
+	printf("Found columns in %s:\n", FILE_PATH);
+	printStringArray(app->column_names);
+	int row = 0;
+	memset(buffer, 0, BYTES_PER_COLUMN * MAX_COLUMNS);
+	app->row_data = malloc(BYTES_PER_COLUMN * MAX_COLUMNS);
+	while (buffer)
+	{
+		memset(buffer, 0, BYTES_PER_COLUMN * MAX_COLUMNS);
+		fgets(buffer, MAX_COLUMNS * BYTES_PER_COLUMN, stream);
+		if (strcmp(buffer, "") == 0)
+		{
+			break;
+		}
+		row++;
+		app->row_count = row;
+		app->row_data = realloc(app->row_data, row * MAX_COLUMNS * BYTES_PER_COLUMN);
+		char *reader = buffer;
+		for (int i = 0; i < app->column_count; i++)
+		{
+			reader = strtok(i == 0 ? buffer : NULL, "|");
+			strcpy(app->row_data + (row - 1) * BYTES_PER_COLUMN * app->column_count + i * BYTES_PER_COLUMN, reader);
+		}
+	}
+	printf("Loaded %d rows, total size: %d bytes.", app->row_count, app->row_count * app->column_count * BYTES_PER_COLUMN);
 	return 0;
 }
-
 int main(int argc, char **argv)
 {
 	int DEBUG = 0;
@@ -35,7 +90,7 @@ int main(int argc, char **argv)
 	{
 		DEBUG = 1;
 	}
-	struct ApplicationMemory *app = {0};
+	struct ApplicationMemory *app = malloc(sizeof(struct ApplicationMemory));
 	FILE *dosya = fopen(FILE_PATH, "r");
 	if (!dosya)
 	{
