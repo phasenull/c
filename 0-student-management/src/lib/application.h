905acea924a257aec11d2f870bbbca67723ddf73
phasenull <55253035+phasenull@users.noreply.github.com>
2026-06-04 22:47:02 +0300

    0 stale commit

--- 0-student-management/src/lib/application.h
+++ 0-student-management/src/lib/application.h
@@ -1,13 +1,22 @@
 typedef enum
 {
-    MAIN_MENU = 0,
-    STUDENT_READ = 1,
-    STUDENT_WRITE = 2,
-    STUDENT_QUERY = 4,
-    PROCESSING = 99,
+    STATE_MAIN_MENU = 0,
+    STATE_STUDENT_READ = 1,
+    STATE_STUDENT_WRITE = 2,
+    STATE_STUDENT_QUERY = 4,
+    STATE_PROCESSING = 99,
 } APPLICATION_STATE;
 typedef struct
 {
     APPLICATION_STATE state;
-    void *memory;
 } ApplicationState;
+const *STATE_DESCRIPTIONS[] = {
+    "~/students/",
+    "~/students/read",
+    "~/students/%d/write",
+    "~/students/query",
+    "[...]"};
+char *getStateDescriptionFromState(APPLICATION_STATE state)
+{
+    return STATE_DESCRIPTIONS[state];
+}
--- 0-student-management/src/main.c
+++ 0-student-management/src/main.c
@@ -1,4 +1,39 @@
 #include "stdio.h"
+#include "lib/application.h"
+typedef enum
+{
+    ACTION_RETURN = 0,
+    ACTION_STUDENT_READ = 1,
+    ACTION_STUDENT_WRITE = 2,
+    ACTION_STUDENT_QUERY = 3,
+} ACTIONS;
+
+char *DESCRIPTIONS[] = {
+    "Go back",
+    "Read student",
+    "Write student",
+    "Query student"};
+char *getActionDescriptionFromAction(ACTIONS action)
+{
+    return DESCRIPTIONS[action];
+}
+void printActionsList()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        printf("[%d]: %s\n", DESCRIPTIONS[i]);
+    }
+}
 int main()
 {
+    ApplicationState state = {
+        .state = STATE_MAIN_MENU};
+
+    while (1)
+    {
+        int action;
+        printf(getStateDescriptionFromState(state.state));
+        printf("> ");
+        scanf("%d", &action);
+    }
 }
